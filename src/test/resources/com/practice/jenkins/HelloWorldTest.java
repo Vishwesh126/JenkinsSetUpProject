4ec889442ee2ea8748b5a8c8fe25a8a38fee1205
M1046772 <M1046772@A2ML27925.mindtree.com>
2018-11-25 17:01:29 +0530

    Adding Resources to test folder and removing from the resources folder

---
+++ src/test/resources/com/practice/jenkins/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package com.practice.jenkins;
+
+public class HelloWorldTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Jenkins Practice with Hello World Program");
+		System.out.println("Hello World");		
+		
+	}
+
+}
